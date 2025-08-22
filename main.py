43d30ef1ab40e4b03f8ebb574f1659c6d0bbd6d5
Rruvri <ravinathwani@btopenworld.com>
2025-08-22 20:03:42 +0100

    added asteroids + movement

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 from constants import *
 from player import *
+from asteroidfield import *
 
 updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+
 Player.containers = (updatable, drawable)
+Asteroid.containers = (asteroids, updatable, drawable)
+AsteroidField.containers = (updatable)
 
 clock = pygame.time.Clock()
 dt = 0
 player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+AsteroidField()
 
 
 
