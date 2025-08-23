169aecdae42f3b84395c451a55ee3ef3a68c5bc2
Rruvri <ravinathwani@btopenworld.com>
2025-08-23 15:48:36 +0100

    added bullet timer

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -22,6 +23,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_timer -= dt
 
         if keys[pygame.K_w]:
             self.move(dt)
@@ -32,6 +34,8 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             self.rotate(dt)
         if keys[pygame.K_SPACE]:
+            if self.shot_timer > 0:
+                return
             self.shoot()
 
     def rotate(self, dt):
@@ -44,4 +48,5 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position, self.radius)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer = PLAYER_SHOOT_COOLDOWN
 
